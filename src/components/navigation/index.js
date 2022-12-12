98a823b5c6f8e223d361ce6968d843be002c8b96
voidoperator <julionunez@me.com>
2022-12-11 19:53:44 -0500

    feat: replace nav links anchor tags with navlink components

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { getPetTypes } from '../../api/petfinder';
 import Logo from '../../assets/logo.svg';
 import Search from '../search';
@@ -23,22 +24,20 @@ const Navigation = () => {
       </div>
       <ul className="nav-links">
         <li key="all">
-          {/* This link should have an activeClassName and exact prop */}
-          <a href="/" className="nav-link">
+          <NavLink to="/" className="nav-link">
             All Pets
-          </a>
+          </NavLink>
         </li>
         {petTypes
           ? petTypes.map((type) => (
               <li key={type.name}>
-                {/* These links should have an activeClassName prop */}
-                <a
-                  href={`/${type._links.self.href.split('/').pop()}`}
+                <NavLink
+                  to={`/${type._links.self.href.split('/').pop()}`}
                   key={type.name}
                   className="nav-link"
                 >
                   {type.name}s
-                </a>{' '}
+                </NavLink>{' '}
               </li>
             ))
           : 'Loading...'}
